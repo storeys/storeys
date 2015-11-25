296443bbd9ed3a35f09020ab7cded0587090ac13
Thomas @ BeeDesk <thomas@beedesk.com>
2015-11-24 17:34:52 -0800

    ☑︎ Added css loading to TemplateView.

--- storeys/js/views/generic/base.js
+++ storeys/js/views/generic/base.js
@@ -1,6 +1,6 @@
 define(
-    ['require', 'module', 'storeys/template/response'],
-    function(require, module, response) {
+    ['require', 'module', 'storeys/template/response', 'storeys/resources/css', 'storeys/utils/promise'],
+    function(require, module, response, css, Q) {
       var TemplateResponse = response.TemplateResponse;
 
       // -------------------------------------------
@@ -18,26 +18,33 @@ define(
       // ===========================================
       return {
         TemplateView: {
-          as_view: function(templatepath, context, document) {
-            return function(req, params, res) {
-              var template = TemplateResponse(templatepath, context);
+          as_view: function(config) {
+            var templatepath = config.templatepath,
+                context = config.context,
+                document = config.document;
 
-              template(req, params, function(response) {
-                if ('status' in response && response.status === 200) {
-                  if (document) {
-                    window.document.write(response.content);
-                    window.document.close();
-                  } else {
-                    window.document.body.innerHTML = response.content;
-                  }
-                  res({
-                    status: 204,
-                    content: response.content
+            return function(req, params, res) {
+              css.load(config.csspaths)
+                .then(function() {
+                  var template = TemplateResponse(templatepath, context);
+
+                  template(req, params, function(response) {
+                    if ('status' in response && response.status === 200) {
+                      if (document) {
+                        window.document.write(response.content);
+                        window.document.close();
+                      } else {
+                        window.document.body.innerHTML = response.content;
+                      }
+                      res({
+                        status: 204,
+                        content: response.content
+                      });
+                    } else {
+                      res(response);
+                    }
                   });
-                } else {
-                  res(response);
-                }
-              });
+                });
             }
           }
         }
